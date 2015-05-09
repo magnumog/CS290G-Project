0c544c7915078668f47a8e918aca02d619c60cd0
magnumog <magnumog@stud.ntnu.no>
2015-05-09 15:44:16 -0700

    read from file

--- AES/AES.cpp
+++ AES/AES.cpp
@@ -2,10 +2,36 @@
 //
 
 #include "stdafx.h"
+#include <vector>
+#include <math.h>
+#include <iostream>
+#include <fstream>
+#include <memory>
+
+using namespace std;
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	vector<int> arr;
+
+	int length;
+	auto text = ReadAllBytes("", &length);
+
 	return 0;
+}
+
+
+static shared_ptr<char*> ReadAllBytes(const char* filename, int* read)
+{
+	ifstream ifs(filename, ios::binary | ios::ate);
+	ifstream::pos_type pos = ifs.tellg();
+	int length = pos;
+	char* pChars = new char[length];
+	ifs.seekg(0, ios::beg);
+	ifs.read(pChars, length);
+	ifs.close();
+	*read = length;
+	return make_shared<char*>(pChars);
 }
 
